f685a7964afbd3ece7619c1742b771c33de1d068
unknown <Asus@Asus-Bilgisayar.(none)>
2015-05-11 21:26:51 +0300

    Initial commit

--- src/main/java/tr/com/serkanozal/jemstone/Jemstone.java
+++ src/main/java/tr/com/serkanozal/jemstone/Jemstone.java
@@ -16,7 +16,14 @@
 
 package tr.com.serkanozal.jemstone;
 
+import java.util.Date;
+
+import sun.jvm.hotspot.gc_interface.CollectedHeap;
+import tr.com.serkanozal.jemstone.sa.HotSpotServiceabilityAgentContext;
 import tr.com.serkanozal.jemstone.sa.HotSpotServiceabilityAgentManager;
+import tr.com.serkanozal.jemstone.sa.HotSpotServiceabilityAgentParameter.NoHotSpotServiceabilityAgentParameter;
+import tr.com.serkanozal.jemstone.sa.HotSpotServiceabilityAgentResult;
+import tr.com.serkanozal.jemstone.sa.HotSpotServiceabilityAgentWorker;
 import tr.com.serkanozal.jemstone.sa.impl.HotSpotServiceabilityAgentManagerImpl;
 
 public class Jemstone {
@@ -38,16 +45,80 @@ public class Jemstone {
         
 		System.out.println(hotSpotServiceabilityAgentManager.details());
 		
+		/////////////////////////////////////////////////////////////////////////////////
+		
 		System.out.println(hotSpotServiceabilityAgentManager.getCompressedReferences());
 		
-		Foo[] fooArray = new Foo[1000];
-		for (int i = 0; i < fooArray.length; i++) {
-		    fooArray[i] = new Foo();
+		/////////////////////////////////////////////////////////////////////////////////
+		
+		System.out.println("Before instance creation: " + 
+		                   hotSpotServiceabilityAgentManager.getInstanceCount(Date.class));
+		Date[] array = new Date[1000];
+		for (int i = 0; i < array.length; i++) {
+		    array[i] = new Date();
 		}
-		System.out.println(hotSpotServiceabilityAgentManager.getInstanceCount(Foo.class));
+		System.out.println("After instance creation: " + 
+		                   hotSpotServiceabilityAgentManager.getInstanceCount(Date.class));
+		
+		/////////////////////////////////////////////////////////////////////////////////
+		
+		System.out.println(hotSpotServiceabilityAgentManager.executeOnHotSpotSA(HeapSummaryWorker.class));
 	}
 	
-    static class Foo {
+    @SuppressWarnings("serial")
+    // Can be executed via "HotSpotServiceabilityAgent.executeOnHotSpotSA(HeapSummaryWorker.class);"
+    public static class HeapSummaryResult implements HotSpotServiceabilityAgentResult {
+        
+        private final long start;
+        private final long end;
+        private final long capacity;
+        
+        public HeapSummaryResult() {
+            start = -1;
+            end = -1;
+            capacity = -1;
+        }
+        
+        public HeapSummaryResult(long start, long capacity) {
+            this.start = start;
+            this.end = start + capacity;
+            this.capacity = capacity;
+        }
+        
+        public long getStart() {
+            return start;
+        }
+        
+        public long getEnd() {
+            return end;
+        }
+        
+        public long getCapacity() {
+            return capacity;
+        }
+        
+        @Override
+        public String toString() {
+            return "HeapSummaryResult [" + 
+                    "start=" + "0x" + Long.toHexString(start) +
+                    ", end=" + "0x" + Long.toHexString(end) +
+                    ", capacity=" + capacity + "]";
+        }
+        
+    }
+    
+    @SuppressWarnings("serial")
+    public static class HeapSummaryWorker 
+            implements HotSpotServiceabilityAgentWorker<NoHotSpotServiceabilityAgentParameter, HeapSummaryResult> {
+
+        @Override
+        public HeapSummaryResult run(HotSpotServiceabilityAgentContext context,
+                NoHotSpotServiceabilityAgentParameter param) {
+            CollectedHeap heap = context.getVM().getUniverse().heap();
+            long startAddress = Long.parseLong(heap.start().toString().substring(2), 16);
+            long capacity = heap.capacity();
+            return new HeapSummaryResult(startAddress, capacity);
+        }
         
     }
     
